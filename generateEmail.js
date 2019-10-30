0d64e29d459e942ddaad3e3bd2c22e66e247a598
areeder-zero <57199354+areeder-zero@users.noreply.github.com>
2019-10-30 17:33:31 -0500

    adding hacktoberfest wording
    Added hacktoberfest 2019 wording to the email.

--- generateEmail.js
+++ generateEmail.js
@@ -13,6 +13,7 @@ function generateEmail(type){
   html += '<div id="header" style="width:100%;color: #012169;align:center;"><h1>RMA Confirmation</h1></div>';
   html += '<div>';
   html += '<p>Thank you for contacting the Customer Experience Team!</p>';
+  html += '<p>This email template is for use for Hacktoberfest 2019 - I hope I am not too late for a shirt!</p>';
   html += '<table id="imageholder">';
   html += '<tr>';
   html += '<td><img style="width:50%" src="https://4463904.app.netsuite.com/core/media/media.nl?id=5884446&c=4463904&h=ec80e6bce5a77834d6d1"><br>Correct</td>';
@@ -20,7 +21,7 @@ function generateEmail(type){
   html += '</tr>';
   html += '</table>';
   html += '</div>';
-  var subj = 'RMA Confirmation ' + recNum;
+  var subj = 'Happy Hacktoberfest! ' + recNum;
   nlapiSendEmail(467655, 'andrew@peepers.com', subj, html, null, null, attRecs, attFile, true, null, 'meeps@meepers.com');
   nlapiLogExecution('DEBUG','Progress Indicator','Email Sent');
   }
